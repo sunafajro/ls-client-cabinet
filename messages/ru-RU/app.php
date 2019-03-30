b6e3f563a00d51af56141e22cfe340cd41161608
Evgeny Belkin <sunafajro@gmail.com>
2019-03-31 00:20:41 +0300

    переводы строк

---
+++ messages/ru-RU/app.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'Access for your account was restricted. Please contact your office manager.' => 'Доступ в Ваш личный кабинет был ограничен. Для повторной активации кабинета обратитесть к вашему менеджеру.',
+    'Login' => 'Вход',
+    'Password' => 'Пароль',
+    'Please fill out the following fields to login' => 'Пожалуйста введите ваш логин и пароль, для входа в кабинет',
+    'Username' => 'Логин',
+];
