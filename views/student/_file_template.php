b5cad6b5a647b35f9b2b768e5bc5cee3a3a077b7
Evgeny Belkin <sunafajro@gmail.com>
2020-06-01 00:06:37 +0300

    #9 Фикс стилей

--- views/student/_file_template.php
+++ views/student/_file_template.php
@@ -11,8 +11,8 @@ use yii\web\View;
 ?>
 <div class="<?= is_null($file) ? 'hidden js--file-block-template' : '' ?>" style="display: inline-block; margin-right: 5px">
         <span class="label label-info">
-            <span class="js--file-name" style="cursor: pointer"><?= $file->original_name ?? null ?></span>
-            <i class="fa fa-times js--remove-file"></i>
+            <span class="js--file-name"><?= $file->original_name ?? null ?></span>
+            <i class="fa fa-times js--remove-file" style="cursor: pointer" title="Удалить"></i>
         </span>
     <input type="hidden" name="<?= Html::getInputName($model, 'files[]') ?>" value="<?= $file->id ?? null ?>" />
 </div>
