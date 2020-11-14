f551eb09430a4bd15f7c0f3a0ae73696b3bd9725
Evgeny Belkin <sunafajro@gmail.com>
2020-11-14 09:30:20 +0300

    Добавлен режим "сайт на обслуживании"

--- web/index.php
+++ web/index.php
@@ -4,13 +4,12 @@
 defined('YII_DEBUG') or define('YII_DEBUG', isset($_SERVER['YII_DEBUG']) ? $_SERVER['YII_DEBUG'] : 'true');
 defined('YII_ENV') or define('YII_ENV', isset($_SERVER['YII_ENV']) ? $_SERVER['YII_ENV'] : 'dev');
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
-$config = require __DIR__ . '/../config/web.php';
-
 if (YII_ENV === 'maintenance') {
     echo "В данный момент сайт находится в режиме обслуживания. Попробуйте зайти еще раз позже.";
 } else {
+    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+    $config = require __DIR__ . '/../config/web.php';
     (new yii\web\Application($config))->run();
 }
